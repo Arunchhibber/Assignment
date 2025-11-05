679f0741cf2af83707f3ef5d0394bdfaae68a578
Arunchhibber <arunchhibber0101@gmail.com>
2025-11-04 21:00:48 -0500

    Update in index.html, style.css and script.js with some chnagess

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// simple quote list
 const quotes = [
   "Push your limits, one commit at a time.",
   "Keep it simple and clean.",
   "Every expert was once a beginner.",
   "Code, test, repeat.",
   "Don’t just dream it, code it!",
+  "Debugging is like being a detective in a crime movie",
   "Success is built one small commit at a time.",
+  "Code is poetry.",
   "Think it, build it, improve it."
 ];
 
